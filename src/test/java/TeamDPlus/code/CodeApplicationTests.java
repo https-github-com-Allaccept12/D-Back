2466e6c1ca5181a9636f52a05e116e9e137e3a0d
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-17 02:30:37 +0900

    [FEAT] gitAction contextloads 에러

--- src/test/java/TeamDPlus/code/CodeApplicationTests.java
+++ src/test/java/TeamDPlus/code/CodeApplicationTests.java
@@ -2,7 +2,6 @@ package TeamDPlus.code;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootTest
 class CodeApplicationTests {
