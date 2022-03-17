7a7473d19bc4d615f11d844ff13577f7d5360697
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-17 13:57:30 +0900

    [FIX] contextLoad fail 수정중

--- src/test/java/TeamDPlus/code/CodeApplicationTests.java
+++ src/test/java/TeamDPlus/code/CodeApplicationTests.java
@@ -1,14 +1,14 @@
-package TeamDPlus.code;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-@SpringBootTest
-class CodeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package TeamDPlus.code;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//@SpringBootTest
+//class CodeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
