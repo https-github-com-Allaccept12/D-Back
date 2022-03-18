4de115a904e7851e74a34aaa53191557c1fb1201
keeeeeey <sseioul@naver.com>
2022-03-18 20:29:54 +0900

    [FIX] 대댓글 관련 코드 삭제

--- src/main/java/TeamDPlus/code/domain/post/answer/PostAnswerCommentRepository.java
+++
@@ -1,6 +0,0 @@
-package TeamDPlus.code.domain.post.answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostAnswerCommentRepository extends JpaRepository<PostAnswerComment, Long> {
-}
