5a96a473437b499d357cb99435a950ce0b28eb7e
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-19 02:30:42 +0900

    [FIX]  무중단 배포 시도 #4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,6 @@
 REPOSITORY=/home/ec2-user/dplus
 PROJECT_NAME=code-0.0.1-SNAPSHOT
 
-echo "> Build 파일 복사"
-
-cp ./build/libs/*.jar $REPOSITORY/
-
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl PROJECT_NAME | grep java | awk '{print $1}')
--- scripts/start.sh
+++ scripts/start.sh
@@ -8,12 +8,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/dplus
-PROJECT_NAME=code-0.0.2-SNAPSHOT
-
-echo "> Build 파일 복사"
-echo "> cp ./build/libs/*.jar $REPOSITORY/"
-
-cp ./build/libs/*.jar $REPOSITORY/
+PROJECT_NAME=code-0.0.3-SNAPSHOT
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -30,6 +25,5 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml \
+    -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
