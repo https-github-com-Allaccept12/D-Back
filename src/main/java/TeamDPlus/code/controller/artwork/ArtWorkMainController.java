ce0d25b413b16c61b734896103fb37947ab244c7
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-19 16:25:09 +0900

    [TEST 무중단 배포

--- src/main/java/TeamDPlus/code/controller/artwork/ArtWorkMainController.java
+++ src/main/java/TeamDPlus/code/controller/artwork/ArtWorkMainController.java
@@ -23,7 +23,7 @@ public class ArtWorkMainController {
     @GetMapping("/")
     public ResponseEntity<Success> main(@AuthenticationPrincipal UserDetailsImpl user) {
         if (user == null) {
-            return new ResponseEntity<>(new Success("메인 페이지 안되면 어쩔수",
+            return new ResponseEntity<>(new Success("메인 페이지 check",
                     artworkMainService.mostPopularArtWork(null)), HttpStatus.OK);
         }
         return new ResponseEntity<>(new Success("메인 페이지",
