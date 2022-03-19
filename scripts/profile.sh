ff5a4b5c0d808c88b03964f1af64c7c4178f1a9a
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-19 12:01:51 +0900

    [FIX] 무중단 배포 #5

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,6 +5,8 @@ function find_idle_profile()
     # curl 결과로 연결할 서비스 결정
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
+    echo "연결할 서비스 결정에 따른 사인 : ${RESPONSE_CODE}"
+
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=real2
@@ -16,8 +18,10 @@ function find_idle_profile()
     if [ ${CURRENT_PROFILE} == real1 ]
     then
       IDLE_PROFILE=real2
+      echo "연결되지않은 : ${IDLE_PROFILE}"
     else
       IDLE_PROFILE=real1
+      echo "연결되지않은 : ${IDLE_PROFILE}"
     fi
 
     # bash script는 값의 반환이 안된다.
