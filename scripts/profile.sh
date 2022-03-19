7316f576e27a6c8f7ad2b191ee4e6aaf29817a4e
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-19 15:16:02 +0900

    [FIX] 무중단 배포 #9

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,8 +5,6 @@ function find_idle_profile()
     # curl 결과로 연결할 서비스 결정
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-    echo "연결할 서비스 결정에 따른 사인 : ${RESPONSE_CODE}"
-
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=real2
