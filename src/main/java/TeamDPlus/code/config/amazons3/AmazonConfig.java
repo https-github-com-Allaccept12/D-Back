c7341128146e85c5a351d67712a60d9853ff1b50
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-19 21:56:43 +0900

    [FIX] yml팡리 수정

--- src/main/java/TeamDPlus/code/config/amazons3/AmazonConfig.java
+++ src/main/java/TeamDPlus/code/config/amazons3/AmazonConfig.java
@@ -19,12 +19,10 @@ public class AmazonConfig {
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
     @Bean
     public AmazonS3Client amazonS3Client() {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        String region = "ap-northeast-2";
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
