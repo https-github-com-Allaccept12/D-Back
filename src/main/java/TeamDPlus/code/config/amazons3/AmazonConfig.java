6267d054f3873ef8d5a310e55ea732818406bd87
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-23 18:32:02 +0900

    [FEAT] multipartResolver 추가

--- src/main/java/TeamDPlus/code/config/amazons3/AmazonConfig.java
+++ src/main/java/TeamDPlus/code/config/amazons3/AmazonConfig.java
@@ -8,6 +8,8 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
 public class AmazonConfig {
@@ -27,5 +29,12 @@ public class AmazonConfig {
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSizePerFile(2000000000);
+        return multipartResolver;
     }
 }
