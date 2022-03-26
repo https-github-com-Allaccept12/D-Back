87123446218da2d956c8899ec336adbdc9050a58
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-26 23:29:36 +0900

    [FIX] 테스트코드 임시주석

--- src/test/java/com/code/service/artwork/ArtworkMainPageServiceImplTest.java
+++ src/test/java/com/code/service/artwork/ArtworkMainPageServiceImplTest.java
@@ -1,37 +1,37 @@
-package com.code.service.artwork;
-
-import com.example.dplus.repository.artwork.ArtWorkRepository;
-import com.example.dplus.repository.artwork.image.ArtWorkImageRepository;
-import com.example.dplus.repository.artwork.like.ArtWorkLikesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-
-@SpringBootTest
-@Transactional
-class ArtworkMainPageServiceImplTest {
-
-
-    @Autowired
-    ArtWorkRepository artWorkRepository;
-
-    @Autowired
-    ArtWorkImageRepository artWorkImageRepository;
-
-    @Autowired
-    ArtWorkLikesRepository artWorkLikesRepository;
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
+//package com.code.service.artwork;
+//
+//import com.example.dplus.repository.artwork.ArtWorkRepository;
+//import com.example.dplus.repository.artwork.image.ArtWorkImageRepository;
+//import com.example.dplus.repository.artwork.like.ArtWorkLikesRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//
+//@SpringBootTest
+//@Transactional
+//class ArtworkMainPageServiceImplTest {
+//
+//
+//    @Autowired
+//    ArtWorkRepository artWorkRepository;
+//
+//    @Autowired
+//    ArtWorkImageRepository artWorkImageRepository;
+//
+//    @Autowired
+//    ArtWorkLikesRepository artWorkLikesRepository;
+//
+//
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
