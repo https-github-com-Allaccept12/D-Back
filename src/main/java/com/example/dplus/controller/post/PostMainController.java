28b450f835681998c65c1340ecaf5b0236c4ce4a
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-04-05 14:53:59 +0900

    [TEST]게시글 이미지 파일 업로드

--- src/main/java/com/example/dplus/controller/post/PostMainController.java
+++ src/main/java/com/example/dplus/controller/post/PostMainController.java
@@ -65,7 +65,7 @@ public class PostMainController {
     @PostMapping("")
     public ResponseEntity<Success> createPost(@AuthenticationPrincipal UserDetailsImpl user,
                                               @RequestPart PostRequestDto.PostCreate data,
-                                              @RequestPart(required = false) List<MultipartFile> imgFile) {
+                                              @RequestPart List<MultipartFile> imgFile) {
 
         if (user != null) {
             return new ResponseEntity<>(new Success("디플 게시물 등록",
