906822cc2d567bf3b8ce319137bca9b6a3e4f58e
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-04-05 23:04:58 +0900

    [FEAT] 이미지 다 똑같은거만 뿌려주는거 수정

--- src/main/java/com/example/dplus/repository/artwork/image/ArtWorkImageRepository.java
+++ src/main/java/com/example/dplus/repository/artwork/image/ArtWorkImageRepository.java
@@ -2,12 +2,15 @@ package com.example.dplus.repository.artwork.image;
 
 import com.example.dplus.domain.artwork.ArtWorkImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ArtWorkImageRepository extends JpaRepository<ArtWorkImage, Long>{
 
-    List<ArtWorkImage> findByArtWorksId(Long artWorkId);
+    @Query("select art from ArtWorkImage art where art.id =:artWorkId")
+    List<ArtWorkImage> findByArtWorksId(@Param("artWorkId") Long artWorkId);
     void deleteAllByArtWorksId(Long artWorkId);
     void deleteByArtworkImg(String artWorkImg);
 
