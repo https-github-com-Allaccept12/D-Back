ff6e6b1e2fdc7148eef46d155c1324844e143910
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-04-07 01:16:08 +0900

    [TEST] 스크립트 확인

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,7 +5,7 @@ function find_idle_profile()
     # curl 결과로 연결할 서비스 결정
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-    echo "${RESPONSE_CODE}"
+
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
@@ -13,6 +13,8 @@ function find_idle_profile()
     else
         CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
+
+    echo "${CURRENT_PROFILE}"
 
     # IDLE_PROFILE : nginx와 연결되지 않은 profile
     if [ ${CURRENT_PROFILE} == real1 ]
