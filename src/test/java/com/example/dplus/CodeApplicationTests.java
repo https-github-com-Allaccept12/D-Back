2c419b98149e444d5c01afe6ccd7e2d5c9f77c55
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-04-07 01:48:40 +0900

    [FEAT] testcode 브랜치

---
+++ src/test/java/com/example/dplus/CodeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.dplus;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
